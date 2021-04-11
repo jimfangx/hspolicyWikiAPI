300b766ceb1092721b8486b1dc55282becb4dc88
AirFusion <jfang.cv.ca.us@gmail.com>
2021-04-10 20:29:42 -0700

    implemented - see desc - need to test & deploy
    https://app.gitkraken.com/glo/view/card/743fd9c3a056475ca86c871a1db41cd7

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const superagent = require('superagent');
 // const apiKey = require('./apiKeys.json')
 const fs = require('fs')
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+var parseString = require('xml2js').parseString;
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -149,29 +150,185 @@ app.post('/roundreports', (req, resApp) => {
 })
 
 
-app.post('/latestEntry', (req, resApp) => {
-    console.log(req.body.link)
+app.post('/test', async (req, resApp) => {
+    // superagent
+    //     .get(`https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/College%20Prep/pages/Gu-Huang%20Aff/objects/Caselist.CitesClass/0/properties`)
+    //     .end((err, res) => {
+    //         parseString(Buffer.from(res.body).toString(), function (err1, parsed) {
+    //             resApp.send(parsed)
+    //         })
+    //     })
+    var a = {
+        "number": "0",
+        "entryDate": null,
+        "judge": null,
+        "opponent": null,
+        "parentRound": null,
+        "round": null,
+        "entryTitle": null,
+        "tournament": null,
+        "text": "Plan—1AC\nPlan: The United States federal government should establish a mandatory minimum sentence for corporate espionage. \nAdvtg 1\nChinese espionage increasing now~-~--only criminal punishment solves\nCatherine Dunn, 9-21-2018, \"Philly’s U.S. attorney pursuing corporate espionage and white collar crime with ‘100 percent support’ from Jeff Sessions,\" https://www.inquirer, https://www.inquirer.com/philly/business/phillys-u-s-attorney-espionage-trade-secrets-china-jeff-sessions-20180921.html//rLiu\n\nScenario 1 is the Space Race\n\nCorporate espionage crime is a huge danger and empowers Russian-Chinese great power competition – deterring US employees from switching sides and ensuring maximum security is key\nStratfor, 11-5-2019, \"An Era of Unparalleled Espionage Risk Is Upon Us,\" https://worldview.stratfor.com/article/era-unparalleled-corporate-espionage-risk-upon-us-security-cybersecurity-threat//rLiu\nSpecifically, corporate espionage wins China the space race\nNicholas Eftimiades, 7-8-2020, \"The Impact of Chinese Espionage on the United States,\" The Diplomat, https://thediplomat.com/2018/12/the-impact-of-chinese-espionage-on-the-united-states//rLiu\nThe space race is key to hegemony – losing it would guarantee Chinese domination of the world and space, but protecting US intellectual property can solve\nGreg Autry, Steve Kwast, 12-8-2018, \"America Is Losing the Second Space Race to China,\" Foreign Policy, https://foreignpolicy.com/2019/08/22/america-is-losing-the-second-space-race-to-china//rLiu\nThe militarization of space is key to global power projection. \nElbridge Colby 16. Robert M. Gates Senior Fellow at the Center for a New American Security (CNAS). Previously he served for over five years in the U.S. government, primarily in positions focusing on nuclear weapons, arms control, and intelligence reform. He has also served as a staff member or advisor to several governmental commissions, including the 2009 Congressional Strategic Posture Commission and the 2014 National Defense Panel, and serves as a consultant to a variety of U.S. government entities on a range of defense and intelligence matters. “FROM SANCTUARY TO BATTLEFIELD: A Framework for a U.S. Defense and Deterrence Strategy for Space”. https://www.files.ethz.ch/isn/195913/CNAS20Space20Report_16107.pdf.//*recut by rLiu\nChinese hegemony causes war in the SCS and ECS—even low-level conflicts escalate to nuclear war\nO’Hanlon and Poling 20 Michael O’Hanlon, senior fellow and director of research in Foreign Policy at the Brookings Institution, author of The Senkaku Paradox: Risking Great Power War Over Small Stakes, Gregory Poling, senior fellow for Southeast Asia and director of the Asia Maritime Transparency Initiative at CSIS, “ROCKS, REEFS, AND NUCLEAR WAR,” Asia Maritime Transparency Initiative, 1/14/2020, https://amti.csis.org/rocks-reefs-and-nuclear-war/\nScenario 2 is Business Innovation\nIndependently, China is targeting the US energy sector~-~--discouraging energy innovation.\nHunt 19 – Sarah Hunt is a leader in conservative clean energy policy and a successful social entrepreneur. As co-founder and CEO of the Joseph Rainey Center for Public Policy.\n(Sarah Hunt, 2019, “Chinese Energy Innovation Espionage: What’s at stake and what we can do about it?,” Protego Press, https://protegopress.com/chinese-energy-innovation-espionage/.)\nClean energy innovation is key to solving climate change.\nUS DOE 20 – US Department of Energy is a cabinet-level department of the United States Government concerned with the United States' policies regarding energy and safety in handling nuclear material.\n(US DOE, 2020, “What is Mission Innovation?,” US Department of Energy, https://www.energy.gov/what-mission-innovation.)\nWarming causes extinction—CO2 feedback loops, water scarcity, and natural disasters\nKareiva and Carranza 18 Peter Kareiva, PhD, Director of the Institute of the Environment and Sustainability at UCLA, Pritzker Distinguished Professor in Environment and Sustainability at UCLA, Valerie Carranza, PhD student, September 2018, “Existential Risk Due To Ecosystem Collapse: Nature Strikes Back,” Futures, Vol. 102, https://doi.org/10.1016/j.futures.2018.01.001\nEconomic decline causes great power war – nuclear deterrence isn’t enough \nStein Tønnesson 8-20-15 (Tønnesson is a research professor (PRIO) and adjunct professor at the Department of Peace and Conflict Research, Uppsala University. “Deterrence, interdependence and Sino–US peace,” pg. 309-310, International Area Studies Review, https://journals-sagepub-com.dartmouth.idm.oclc.org/doi/10.1177/2233865915596660 //KDCC)\nAdvtg 2\nCorporate espionage escalates US-China tensions, especially fueling the trade war~-~--Covid exacerbates.\nWu 8/6 – Chu Wu is an International Broadcaster (Multimedia) (Mandarin) at VOA News.\n(Chu Wu, August 6, 2020, “Tensions Mount over China’s Industrial Espionage in US,” Voice of America News, https://www.voanews.com/east-asia-pacific/voa-news-china/tensions-mount-over-chinas-industrial-espionage-us.)\nAn end to trade secret theft ends the trade war.\nYeung and Leng 19 – Karen Yeung joined the Post in 2017 after more than 15 years' experience on global newswires in Hong Kong and Shanghai. She spent eight years in Shanghai and has received awards for best feature, analysis and agenda-setting. Sidney Leng joined the Post in 2015 after spending a year and a half working for US media, including National Public Radio and Foreign Policy Magazine. He has been covering China's macroeconomic policies and financial regulations since 2016.\n(Karen Yeung and Sidney Leng, February 25, 2019, “Can China Meet US Trade War Demands On IP Theft And Forced Technology Transfer?,” South China Morning Post, https://www.scmp.com/economy/china-economy/article/2187312/us-china-trade-war-can-china-meet-us-demands-ip-theft-and.)\nThe trade war collapses the US manufacturing sector~-~--it’s stalled in the status quo.\nSwanson and Smialek 1/3 – Ana Swanson reported from Washington, and Jeanna Smialek from San Diego. Alan Rappeport contributed reporting from Washington, and Peter Eavis and Matt Phillips from New York. She is based in the Washington bureau and covers trade and international economics for The New York Times. She previously worked at The Washington Post, where she wrote about trade, the Federal Reserve and the economy. Jeanna Smialek writes about the Federal Reserve and the economy for The New York Times. She previously covered economics at Bloomberg News, where she also wrote feature stories for Businessweek magazine.\nThe manufacturing sector is key to economic growth. AND, the industry is not at its worst but getting worse because of the trade war. \nHopkins 1/6 – Christopher A. Hopkins, CFA, is a vice president and portfolio manager for Barnett and Co. in Chattanooga.\n(Christopher A. Hopkins, January 6, 2020, “Personal Finance: Manufacturing Remains a Key to US Economy,” Chattanooga Times Free Press, https://www.timesfreepress.com/news/business/aroundregion/story/2020/jan/06/manufacturing-remains-key-us-economy/512329/.)\nFor the fifth month on a row, U.S. industrial production contracted in December reflecting a number of challenges including trade tensions, dollar strength and a general slowdown in global production. The Institute for Supply Management monthly gauge of factory activity fell to 47.2 for the final month of 2019, the lowest reading since June 2009. The ISM measure is a \"diffusion index,\" calibrated such that a level of 50 is neutral. \nEconomic decline causes great power war – nuclear deterrence isn’t enough \nStein Tønnesson 8-20-15 (Tønnesson is a research professor (PRIO) and adjunct professor at the Department of Peace and Conflict Research, Uppsala University. “Deterrence, interdependence and Sino–US peace,” pg. 309-310, International Area Studies Review, https://journals-sagepub-com.dartmouth.idm.oclc.org/doi/10.1177/2233865915596660 //KDCC)\nSolvency—1AC\nMandatory minimums for corporate espionage are necessary to deter trade secret theft—civil penalties alone are insufficient\nByrdsong 15 Danielle K Byrdsong, JD candidate, “Keeping the Best Kept Secrets: Mandatory Minimum Sentencing for Trade Secret Theft Under the Economic Espionage Act,” New England Journal on Criminal and Civil Confinement 41, no. 2, Spring 2015, https://heinonline.org/HOL/LandingPage?handle=hein.journals/nejccc41anddiv=27andid=andpage=//cb\nLegislative branch is best to define and grade crimes – critical to countering white-collar crime\nBaer 19 (Meriam, visiting professor of law @ Yale, December 2019, “Sorting Out White-Collar Crime” Texas Law Review – Volume 97 – Issue 2, https://texaslawreview.org/sorting-out-white-collar-crime/)",
+        "roundReport": null,
+        "propertyLink": "https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/College%20Prep/pages/Gu-Huang%20Aff/objects/Caselist.CitesClass/0/properties"
+    }
 
-    // var reqLink = req.body.link.replace('https://hspolicy.debatecoaches.org/', "").split('/')
-    // reqLink = `https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/${reqLink[0]}/pages/${reqLink[1]}/objects` // this has to change every year :( cause of the hspolicy20 thing
 
-    var reqLink = `https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/College%20Prep/pages/Gu-Huang%20Aff/objects`
-    console.log(reqLink)
+
+    console.log(await getProp(a))
+    async function getProp(entryObj) {
+        return new Promise(async (resolve, reject) => {
+            var internalObj = entryObj
+            console.log("wave")
+            console.log(internalObj)
+            console.log(entryObj.propertyLink)
+            superagent
+                .get(entryObj.propertyLink)
+                .end((err, res) => {
+                    parseString(Buffer.from(res.body).toString(), function (parseErr, parsed) {
+                        // internalObj.entryDate = 
+                        for (i = 0; i < parsed.properties.property.length; i++) {
+                            switch (parsed.properties.property[i].$.name) {
+                                case 'EntryDate':
+                                    internalObj.entryDate = parsed.properties.property[i].value[0]
+                                case 'Judge':
+                                    internalObj.judge = parsed.properties.property[i].value[0]
+                                case 'Opponent':
+                                    internalObj.opponent = parsed.properties.property[i].value[0]
+                                case 'Round':
+                                    internalObj.round = parsed.properties.property[i].value[0]
+                                case 'RoundReport':
+                                    internalObj.roundReport = parsed.properties.property[i].value[0]
+                                case 'ParentRound':
+                                    internalObj.parentRound = parsed.properties.property[i].value[0]
+                                case 'Title':
+                                    internalObj.entryTitle = parsed.properties.property[i].value[0]
+                                case 'RoundReport':
+                                    internalObj.roundReport = parsed.properties.property[i].value[0]
+                                case 'Tournament':
+                                    internalObj.tournament = parsed.properties.property[i].value[0]
+                            }
+                        }
+                        resolve(internalObj)
+
+                    })
+                })
+        })
+
+    }
+})
+
+app.post('/latestEntry', async (req, resApp) => {
+
+    var reqLink = req.body.link.replace('https://hspolicy.debatecoaches.org/', "").split('/')
+    reqLink = `https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/${reqLink[0]}/pages/${reqLink[1]}/objects` // this has to change every year :( cause of the hspolicy20 thing
+
+    // var reqLink = `https://hspolicy.debatecoaches.org/rest/wikis/hspolicy20/spaces/College%20Prep/pages/Gu-Huang%20Aff/objects`
     superagent
         .get(reqLink)
-        // .redirects(0)
-        .end((err, res) => {
-            console.log(Buffer.from(res.body).toString())
-            $ = cheerio.load(Buffer.from(res.body).toString(), {
-                xmlMode: true,
-                xml: true
-            });
+        .end(async (err, res) => {
+            parseString(Buffer.from(res.body).toString(), async function (parseErr, parsed) {
+                var entryList = []
 
-            // console.log($('\\objects').children().length)
-            for (i = 0; i < $('\\objects').children().length; i++) {
-                
-            }
+                entryList = await setupProp(parsed)
+
+                entryList = await Promise.all(
+                    entryList.map(async (currentValue) => {
+                        return (await getProp(currentValue))
+                    })
+                )
+
+                entryList = await mergeEntries(entryList)
+                resApp.send(entryList)
+
+                async function mergeEntries(entryList) {
+                    return new Promise(async (resolve, reject) => {
+                        for (i = 0; i < entryList.length; i++) {
+                            for (j = 0; j < entryList.length; j++) {
+                                if ((entryList[i].number === entryList[j].number) && (i != j)) { // match - we have a duplicate, one has a cite, the other one does not
+                                    if (entryList[i].propertyLink.includes('CitesClass')) { // i has more info (delete j)
+                                        entryList.splice(j, 1)
+                                    } else if (entryList[j].propertyLink.includes('CitesClass')) { // j has more info (delete i)
+                                        entryList.splice(i, 1)
+                                    }
+                                }
+                            }
+                        }
+                        resolve(entryList)
+                    })
+                }
+
+                async function getProp(entryObj) {
+                    return new Promise(async (resolve, reject) => {
+                        var internalObj = entryObj
+                        superagent
+                            .get(entryObj.propertyLink)
+                            .end((err, res) => {
+                                parseString(Buffer.from(res.body).toString(), function (parseErr, parsed) {
+
+                                    for (i = 0; i < parsed.properties.property.length; i++) {
+                                        switch (parsed.properties.property[i].$.name) {
+                                            case 'EntryDate':
+                                                internalObj.entryDate = parsed.properties.property[i].value[0]
+                                            case 'Judge':
+                                                internalObj.judge = parsed.properties.property[i].value[0]
+                                            case 'Opponent':
+                                                internalObj.opponent = parsed.properties.property[i].value[0]
+                                            case 'Round':
+                                                internalObj.round = parsed.properties.property[i].value[0]
+                                            case 'RoundReport':
+                                                internalObj.roundReport = parsed.properties.property[i].value[0]
+                                            case 'ParentRound':
+                                                internalObj.parentRound = parsed.properties.property[i].value[0]
+                                            case 'Title':
+                                                internalObj.entryTitle = parsed.properties.property[i].value[0]
+                                            case 'RoundReport':
+                                                internalObj.roundReport = parsed.properties.property[i].value[0]
+                                            case 'Tournament':
+                                                internalObj.tournament = parsed.properties.property[i].value[0]
+                                        }
+                                    }
+                                    resolve(internalObj)
+
+                                })
+                            })
+                    })
+                }
+            })
+
         })
+
+    async function setupProp(parsed) {
+        return new Promise(async (resolve, reject) => {
+            var entryList = []
+            var entryObj;
+            for (i = 0; i < parsed.objects.objectSummary.length; i++) {
+                entryObj = {
+                    "number": null, // check
+                    "entryDate": null, //ok
+                    "judge": null, //ok
+                    "opponent": null, //ok
+                    "parentRound": null, //ok
+                    "round": null, //ok
+                    "entryTitle": null, //ok
+                    "tournament": null, //ok
+                    "text": null, // check aka cites
+                    "roundReport": null,
+                    "propertyLink": null // check
+                }
+                entryObj.number = parsed.objects.objectSummary[i].number[0]
+                entryObj.propertyLink = parsed.objects.objectSummary[i].link[1].$.href
+                if (parsed.objects.objectSummary[i].headline[0].length > 4) {
+                    entryObj.text = parsed.objects.objectSummary[i].headline[0]
+                }
+
+                entryList.push(entryObj)
+            }
+
+            resolve(entryList)
+
+        })
+
+    }
 
 
 })
